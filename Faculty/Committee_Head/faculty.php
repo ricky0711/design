fa6aaa3eaa1f76f6397ba6f5115e3c521eb97d53
Ricky0711 <rickysharma0711@gmail.com>
2020-06-19 00:46:49 +0530

    faculty

--- Faculty/Committee_Head/faculty.php
+++ Faculty/Committee_Head/faculty.php
@@ -30,7 +30,117 @@
                     </p>
                     </div>
                     <div class="card-body">
-                        
+                                <div class="toolbar">
+                                <!--        Here you can write extra buttons/actions for the toolbar              -->
+                                </div>
+                                <?php
+                                    include('../../Files/PDO/dbcon.php');	
+                                    $stmt=$con->prepare("CALL VIEW_COMMITTEE_MEMBER();");
+                                    $stmt->execute();
+                                ?>
+                            <div class="material-datatables">
+                                <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th></th>
+                                            <th><h4>Committee Members</h4></th>
+                                            <th></th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                        <tr>
+                                        <td>Name</td>
+                                        <td>Email</td>
+                                        <td>Phone Number</td>
+                                        <td>Gender</td>
+                                        <td>About</td>
+                                        <th class="disabled-sorting pull-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                        <td>Name</td>
+                                        <td>Email</td>
+                                        <td>Phone Number</td>
+                                        <td>Gender</td>
+                                        <td>About</td>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php while($data = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                                        <tr>
+                                        <td><?php echo $data['FACULTY_FIRST_NAME']; ?> <?php echo $data['FACULTY_LAST_NAME']; ?></td>
+                                        <td class="text-nowrap"><?php echo $data['FACULTY_EMAIL']; ?></td>
+                                        <td><?php echo $data['FACULTY_PHONE_NUMBER']; ?></td>
+                                        <td><?php echo $data['FACULTY_GENDER']; ?></td>
+                                        <td><?php echo $data['FACULTY_ABOUT']; ?></td>
+                                        <td><a href="view_faculty_profile.php?fid=<?php echo $data['FACULTY_ID'] ?>" title="">
+                                        <button type="button" class="btn btn-outline-info"><i class="fa fa-user-circle-o"></i></button>
+                                        </a></td>
+                                        <td><a href="deactivate_profile.php?fid=<?php echo $data['FACULTY_ID'] ?>" title="">
+                                        <button type="button" class="btn btn-outline-danger"><i class="fas fa-user-slash"></i></button>
+                                        </a></td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <?php 
+                                $stmt=$con->prepare("CALL VIEW_FACULTY();");
+                                $stmt->execute();
+                                $stmt=$con->prepare("CALL VIEW_FACULTY();");
+                                $stmt->execute();
+                            ?> <br><br><br><br>
+                            <div class="material-datatables">
+                                <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th></th>
+                                            <th><h4>Faculty Members</h4></th>
+                                            <th></th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                        <tr>
+                                        <td>Name</td>
+                                        <td>Email</td>
+                                        <td>Phone Number</td>
+                                        <td>Gender</td>
+                                        <td>About</td>
+                                        <th class="disabled-sorting pull-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                        <td>Name</td>
+                                        <td>Email</td>
+                                        <td>Phone Number</td>
+                                        <td>Gender</td>
+                                        <td>About</td>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php while($data = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                                        <tr>
+                                        <td><?php echo $data['FACULTY_FIRST_NAME']; ?> <?php echo $data['FACULTY_LAST_NAME']; ?></td>
+                                        <td class="text-nowrap"><?php echo $data['FACULTY_EMAIL']; ?></td>
+                                        <td><?php echo $data['FACULTY_PHONE_NUMBER']; ?></td>
+                                        <td><?php echo $data['FACULTY_GENDER']; ?></td>
+                                        <td><?php echo $data['FACULTY_ABOUT']; ?></td>
+                                        <td><a href="view_faculty_profile.php?fid=<?php echo $data['FACULTY_ID'] ?>" title="">
+                                        <button type="button" class="btn btn-outline-info"><i class="fa fa-user-circle-o"></i></button>
+                                        </a></td>
+                                        <td><a href="deactivate_profile.php?fid=<?php echo $data['FACULTY_ID'] ?>" title="">
+                                        <button type="button" class="btn btn-outline-danger"><i class="fas fa-user-slash"></i></button>
+                                        </a></td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
                     </div>
                 </div>
                 </div>
@@ -43,35 +153,60 @@
                     </p>
                     </div>
                     <div class="card-body">
-                        
-                    </div>
-                </div>
-                </div>
-                <div class="tab-pane" id="link9">
-                <div class="card">
-                    <div class="card-header">
-                    <h4 class="card-title">View Resume Details</h4>
-                    <p class="card-category">
-                        More information here
-                    </p>
-                    </div>
-                    <div class="card-body">
-                            
-                   </div>
-                </div>
-                </div>
-                <div class="tab-pane" id="link10">
-                <div class="card">
-                    <div class="card-header">
-                    <h4 class="card-title">Help center</h4>
-                    <p class="card-category">
-                        More information here
-                    </p>
-                    </div>
-                    <div class="card-body">
-                    From the seamless transition of glass and metal to the streamlined profile, every detail was carefully considered to enhance your experience. So while its display is larger, the phone feels just right.
-                    <br>
-                    <br> Another Text. The first thing you notice when you hold the phone is how great it feels in your hand. The cover glass curves down around the sides to meet the anodized aluminum enclosure in a remarkable, simplified design.
+                                <div class="toolbar">
+                                <!--        Here you can write extra buttons/actions for the toolbar              -->
+                                </div>
+                                <?php
+                                    $stmt=$con->prepare("CALL VIEW_DEACTIVE_FACULTY();");
+                                    $stmt->execute();
+                                    $stmt=$con->prepare("CALL VIEW_DEACTIVE_FACULTY();");
+                                    $stmt->execute();
+                                ?>
+                            <div class="material-datatables">
+                                <table id="datatables" class="table table-striped table-no-bordered table-hover" cellspacing="0" width="100%" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th></th>
+                                            <th><h4>Committee Members</h4></th>
+                                            <th></th>
+                                            <th></th>
+                                            <th></th>
+                                        </tr>
+                                        <tr>
+                                        <td>Name</td>
+                                        <td>Email</td>
+                                        <td>Phone Number</td>
+                                        <td>Gender</td>
+                                        <td>About</td>
+                                        <th class="disabled-sorting pull-right">Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                        <td>Name</td>
+                                        <td>Email</td>
+                                        <td>Phone Number</td>
+                                        <td>Gender</td>
+                                        <td>About</td>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php while($data = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                                        <tr>
+                                        <td><?php echo $data['FACULTY_FIRST_NAME']; ?> <?php echo $data['FACULTY_LAST_NAME']; ?></td>
+                                        <td class="text-nowrap"><?php echo $data['FACULTY_EMAIL']; ?></td>
+                                        <td><?php echo $data['FACULTY_PHONE_NUMBER']; ?></td>
+                                        <td><?php echo $data['FACULTY_GENDER']; ?></td>
+                                        <td><?php echo $data['FACULTY_ABOUT']; ?></td>
+                                        <td><a href="restore_faculty.php?fid=<?php echo $data['FACULTY_ID'] ?>" title="">
+                                            <button type="button" class="btn btn-outline-info"><i class="fas fa-trash-restore"></i></button>
+                                            </a></td>
+                                        </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
                     </div>
                 </div>
                 </div>
